a89b7d7e1542cf5a692b4548d046a716021588bb
Victor Chabrol <victor1chabrol@gmail.com>
2025-11-05 17:45:52 +0100

    Commit system_prompt  choice function in parametres_generaux
    Commit LLM_just_text as a benchmark for LLM_text

--- Parametres_generaux.R
+++ Parametres_generaux.R
@@ -1,5 +1,13 @@
 # SCRIPT PARAMETRES GENERAUX
 
+
+rm(list = ls())  
+source("Library_Nowcasting_LLM.R")
+source("LLM_functions.R")
+source("Script_dates_prev.R")
+source("Parametres_generaux.R")
+
+
 ##############################
 # Répertoire de travail actif
 #############################
@@ -10,6 +18,7 @@ here::i_am("Parametres_generaux.R")
 setwd(dirname(getActiveDocumentContext()$path))
 
 load_dot_env('.env')
+
 
 ###################################################
 #INITIALISATION PARAMETRES
@@ -30,5 +39,38 @@ dates <- read_xlsx(here("dates_prev.xlsx")) #possible aussi de prendre df_date (
 
 # API Key (pour ellmer on utilise API_KEY_GEMINI)
 cle_API <- Sys.getenv("API_KEY_GEMINI")
+
+########################################
+# Choix du système prompt
+#######################################
+
+system_prompt <- function(x){
+  if (x == "Text"){
+    txt_prompt <- ifelse(english == 1,
+                         "You will act as the economic agent you are told to be. Answer based on your knowledge and the document provided in less than 200 words. You will use only the information available as of the forecast date, do not invent facts." ,
+                         "Vous allez incarner des agents économiques spécifiés. Répondez aux questions en moins de 200 mots, à l'aide de vos connaissances et du document fourni. Vous n'utiliserez que l'information disponible à la date du jour de la prévision, n'inventez pas de faits."
+    )
+  }else if (x == "noText"){
+    txt_prompt <- ifelse(english == 1,
+                         "You will act as the economic agent you are told to be. Answer based on your knowledge and researches in less than 200 words, do not invent facts." ,
+                         "Vous allez incarner des agents économiques spécifiés. Répondez aux questions en moins de 200 mots à l'aide de vos connaissances et de vos recherches, n'inventez pas de faits.")
+    
+  }else if(x== "all"){
+    txt_prompt <- ifelse(english == 1,
+                         "You will act as the economic agent you are told to be. Answer based on your knowledge, the document provided and the information provided in the prompt in less than 200 words. You will use only the information available as of the forecast date, do not invent facts." ,
+                         "Vous allez incarner des agents économiques spécifiés. Répondez aux questions en moins de 200 mots, à l'aide de vos connaissances, du document fourni et des informations contenues dans le prompt utilisateur. Vous n'utiliserez que l'information disponible à la date du jour de la prévision, n'inventez pas de faits."
+    )
+    
+  }else if(x== "justText"){
+    txt_prompt <- ifelse(english == 1,
+                         "You will act as the economic agent you are told to be. Answer based solely on the document provided in less than 200 words. Do not invent facts." ,
+                         "Vous allez incarner des agents économiques spécifiés. Répondez aux questions en moins de 200 mots uniquement à l'aide du document fourni. N'inventez pas de faits."
+    )
+  }
+  return(txt_prompt)
+}
+
+
+
 
 
