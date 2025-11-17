a928d424ba9ba6b99ab0e5be684976b65ea0d5da
mbessec <60462278+mbessec@users.noreply.github.com>
2025-11-17 11:06:31 +0100

    J'ai affiné le traitement des dates pour 2020: on garde les résultats pour les dates de 2020/21 avec EMC pour rolling, je remplace ces 4 dates par _text

--- Ajout_Results2020.R
+++ Ajout_Results2020.R
@@ -2,14 +2,19 @@
 # Fichiers stockés dans le dossier final_results
 
 rm(list=ls())
+source("Library_Nowcasting_LLM.R")
 
 # Importation des résultats
 
-# no_text
-df_BDF_2020 <- read_excel('Results/BDF_noText2.xlsx')
-df_BDF_2020 <- subset(df_BDF_2020,Date>"2019-12-08")
-df_INSEE_2020 <- read_excel('Results/INSEE_noText2.xlsx')
-df_INSEE_2020 <- subset(df_INSEE_2020,Date>"2019-12-08")
+# no_text  => 2020-02 à 07, 2020-09 et 2021-01
+df_BDF_2020 <- read_excel('Results/BDF_noText_sansEMC.xlsx')
+df_INSEE_2020 <- read_excel('Results/INSEE_noText_sansEMC.xlsx')
+
+# si on veut tout remplacer de janv 2020 à janv 2021 (pour rolling) 
+#df_BDF_2020 <- read_excel('Results/BDF_noText2.xlsx')
+#df_INSEE_2020 <- read_excel('Results/INSEE_noText2.xlsx')
+#df_BDF_2020 <- subset(df_BDF_2020,Date>"2019-12-08")
+#df_INSEE_2020 <- subset(df_INSEE_2020,Date>"2019-12-08")
 
 
 #============================================
@@ -97,16 +102,10 @@ write.xlsx(df_final_INSEE, file = "Final_Results/INSEE_all_2020.xlsx",
 
 
 
-# FICHIER VICTOR => format date différents
-
 # =============================================
-# Correction BDF_rolling_text et INSEE_rolling_text
-df_BDF <- read_excel('Results/BDF_rolling_text.xlsx')   #1
-df_INSEE <- read_excel('Results/INSEE_rolling_text.xlsx')   #2
-
-# Pb type de date différent
-df_BDF_2020$Date <- as.POSIXct(df_BDF_2020$Date, tz = "UTC")
-df_INSEE_2020$Date <- as.POSIXct(df_INSEE_2020$Date, tz = "UTC")
+# Correction BDF_just_Text et INSEE_just_Text
+df_BDF <- read_excel('Results/BDF_just_text.xlsx')   #1
+df_INSEE <- read_excel('Results/INSEE_just_text.xlsx')   #2
 
 # remplace les dates communes de 2020 et ajoute les dates manquantes
 df_final_BDF <- df_BDF %>%
@@ -117,6 +116,82 @@ df_final_INSEE <- df_INSEE %>%
   rows_upsert(df_INSEE_2020, by = "Date") %>%
   arrange(Date)
 
+write.xlsx(df_final_BDF, file = "Final_Results/BDF_just_text_2020.xlsx",
+           sheetName = 'prevision', rowNames = FALSE)  #3
+
+write.xlsx(df_final_INSEE, file = "Final_Results/INSEE_just_Text_2020.xlsx",
+           sheetName = 'prevision', rowNames = FALSE)  #4
+
+
+# =============================================
+# Correction BDF_excel_error et INSEE_excel_error
+df_BDF <- read_excel('Results/BDF_excel_error.xlsx')   #1
+df_INSEE <- read_excel('Results/INSEE_excel_error.xlsx')   #2
+
+# remplace les dates communes de 2020 et ajoute les dates manquantes
+df_final_BDF <- df_BDF %>%
+  rows_upsert(df_BDF_2020, by = "Date") %>%
+  arrange(Date)
+
+df_final_INSEE <- df_INSEE %>%
+  rows_upsert(df_INSEE_2020, by = "Date") %>%
+  arrange(Date)
+
+write.xlsx(df_final_BDF, file = "Final_Results/BDF_excel_error_2020.xlsx",
+           sheetName = 'prevision', rowNames = FALSE)  #3
+
+write.xlsx(df_final_INSEE, file = "Final_Results/INSEE_excel_error_2020.xlsx",
+           sheetName = 'prevision', rowNames = FALSE)  #4
+
+
+
+# FICHIER VICTOR => format date différents
+# =============================================
+# Correction BDF_rolling_text et INSEE_rolling_text
+# TRAITEMENT PARTICULIER POUR ROLLING 
+   # => ON REMPLACE TOUT A PARTIR DE 2020-02 PAR _NO_TEXT
+   # => ON REMPLACE les 4 dates avec EMC par _TEXT
+df_BDF <- read_excel('Results/BDF_rolling_text.xlsx')   #1
+df_INSEE <- read_excel('Results/INSEE_rolling_text.xlsx')   #2
+
+# ETAPE 1 : on remplace tout par _no_tex
+  df_BDF_2020 <- read_excel('Results/BDF_noText2.xlsx')
+  df_INSEE_2020 <- read_excel('Results/INSEE_noText2.xlsx')
+  df_BDF_2020 <- subset(df_BDF_2020,Date>"2020-01-09")
+  df_INSEE_2020 <- subset(df_INSEE_2020,Date>"2020-01-09")
+  
+  # Pb type de date différent
+  df_BDF_2020$Date <- as.POSIXct(df_BDF_2020$Date, tz = "UTC")
+  df_INSEE_2020$Date <- as.POSIXct(df_INSEE_2020$Date, tz = "UTC")
+  
+  # remplace les dates communes de 2020 et ajoute les dates manquantes
+  df_final_BDF <- df_BDF %>%
+    rows_upsert(df_BDF_2020, by = "Date") %>%
+    arrange(Date)
+  
+  df_final_INSEE <- df_INSEE %>%
+    rows_upsert(df_INSEE_2020, by = "Date") %>%
+    arrange(Date)
+
+  
+# ETAPE 2 : on affine en remplaçant les 4 dates avec EMC par _text
+  df_BDF_2020bis <- read_excel('Results/BDF_Text_avecEMC.xlsx')
+  df_INSEE_2020bis <- read_excel('Results/INSEE_Text_avecEMC.xlsx')
+
+    # Pb type de date différent
+  df_BDF_2020bis$Date <- as.POSIXct(df_BDF_2020bis$Date, tz = "UTC")
+  df_INSEE_2020bis$Date <- as.POSIXct(df_INSEE_2020bis$Date, tz = "UTC")
+  
+  # remplace les dates communes de 2020 (avec EMC)
+  df_final_BDF <- df_final_BDF %>%
+    rows_upsert(df_BDF_2020bis, by = "Date") %>%
+    arrange(Date)
+  
+  df_final_INSEE <- df_final_INSEE %>%
+    rows_upsert(df_INSEE_2020bis, by = "Date") %>%
+    arrange(Date)
+  
+  
 write.xlsx(df_final_BDF, file = "Final_Results/BDF_rolling_text_2020.xlsx", 
            sheetName = 'prevision', rowNames = FALSE)  #3
 
@@ -124,24 +199,5 @@ write.xlsx(df_final_INSEE, file = "Final_Results/INSEE_rolling_Text_2020.xlsx",
            sheetName = 'prevision', rowNames = FALSE)  #4
 
 
-# # =============================================
-# # Correction BDF_just_Text et INSEE_just_Text 
-# df_BDF <- read_excel('Results/BDF_just_text.xlsx')   #1
-# df_INSEE <- read_excel('Results/INSEE_just_text.xlsx')   #2
-# 
-# # remplace les dates communes de 2020 et ajoute les dates manquantes
-# df_final_BDF <- df_BDF %>%
-#   rows_upsert(df_BDF_2020, by = "Date") %>%
-#   arrange(Date)
-# 
-# df_final_INSEE <- df_INSEE %>%
-#   rows_upsert(df_INSEE_2020, by = "Date") %>%
-#   arrange(Date)
-# 
-# write.xlsx(df_final_BDF, file = "Final_Results/BDF_just_text_2020.xlsx", 
-#            sheetName = 'prevision', rowNames = FALSE)  #3
-# 
-# write.xlsx(df_final_INSEE, file = "Final_Results/INSEE_just_Text_2020.xlsx", 
-#            sheetName = 'prevision', rowNames = FALSE)  #4
 
 
