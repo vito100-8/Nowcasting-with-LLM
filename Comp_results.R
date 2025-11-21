7a04955e028ec2b98ac5583af1f00f3635b57189
Victor Chabrol <victor1chabrol@gmail.com>
2025-11-21 15:16:08 +0100

    Adapt comp_results to monthly results logic : - improved stats and graphs

--- Comp_results.R
+++ Comp_results.R
@@ -11,55 +11,82 @@ source("LLM_functions.R")
 #INPUTS
 ###########################
 
-# Définissez les chemins des fichiers de résultats BDF et INSEE pour le même modèle
+# chemins des fichiers de résultats BDF et INSEE pour le même modèle
 MODEL_NAME <- "No Text" # Nom du modèle pour référence
 FILE_PATH_BDF <- "Final_results/BDF_noText_2020.xlsx" 
 FILE_PATH_INSEE <- "Final_results/INSEE_noText_2020.xlsx" 
+
 
 
 # Préparation des données
 
 message(paste("Analyse comparative pour le modèle:", MODEL_NAME))
 
-wide_BDF <- read_xlsx(FILE_PATH_BDF) |> mutate(Date = as.Date(Date))
-wide_INSEE <- read_xlsx(FILE_PATH_INSEE) |> mutate(Date = as.Date(Date))
+stats_BDF <- read_xlsx(FILE_PATH_BDF) |>
+  rowwise() |>
+  mutate(Date = Date,
+         median_forecast = median(c_across(starts_with("forecast_")), na.rm = TRUE),
+         median_conf = median(c_across(starts_with("confidence_")), na.rm = TRUE),
+         source = "BDF",
+         .keep = "none")
+stats_INSEE <- read_xlsx(FILE_PATH_INSEE) |> 
+  rowwise() |>
+  mutate(Date = Date,
+         median_forecast = median(c_across(starts_with("forecast_")), na.rm = TRUE),
+         median_conf = median(c_across(starts_with("confidence_")), na.rm = TRUE),
+         source = "INSEE",
+          .keep = "none")
 
-# Conversion au format long
-bdf_long <- to_long(wide_BDF, "BDF")
-insee_long <- to_long(wide_INSEE, "INSEE")
-both_long <- bind_rows(bdf_long, insee_long)
+
+
+#bind
+stats_both <- bind_rows(stats_BDF, stats_INSEE)
+
+#mensualisation des données
+stats_both <- stats_both |>
+  mutate(
+    month = month(Date),
+    year = year(Date),
+    forecast_quarter = case_when(
+      month %in% c(2, 3, 4) ~ 1, month %in% c(5, 6, 7) ~ 2,
+      month %in% c(8, 9, 10) ~ 3, month %in% c(11, 12, 1) ~ 4
+    ),
+    month_in_quarter = case_when( 
+      month %in% c(2, 5, 8, 11) ~ 1, # M1 
+      month %in% c(3, 6, 9, 12) ~ 2, # M2 
+      month %in% c(4, 7, 10, 1) ~ 3  # M3 
+    ),
+    Mois_Label = paste0("Mois ", month_in_quarter)
+  )
+
 
 ####################################
 # Stats descriptives
 ####################################
 
 #Stats 
-stats_des <- both_long |>
-  group_by(Date, source) |>
+stats_des <- stats_both |>
+  group_by(Mois_Label, source) |>
   summarise(
-    Moyenne = mean(forecast, na.rm = TRUE),
-    Médiane = median(forecast, na.rm = TRUE),
-    Variance = var(forecast, na.rm = TRUE),
-    EcartType = sd(forecast, na.rm = TRUE),
-    Skewness = skewness(forecast, na.rm = TRUE),
-    Kurtosis = kurtosis(forecast, na.rm = TRUE),
-    Moyenne_Confiance = mean(confidence, na.rm = TRUE),
+    Moyenne = mean(median_forecast, na.rm = TRUE),
+    Médiane = median(median_forecast, na.rm = TRUE),
+    Variance = var(median_forecast, na.rm = TRUE),
+    EcartType = sd(median_forecast, na.rm = TRUE),
+    Skewness = skewness(median_forecast, na.rm = TRUE),
+    Kurtosis = kurtosis(median_forecast, na.rm = TRUE),
+    Moyenne_Confiance = mean(median_conf, na.rm = TRUE),
     Observations = n(),
     .groups = "drop"
   )
 
 
-
-
 # Corrélation et T-Test sur les Moyennes
-df_BDF_means <- wide_BDF |> 
-  rowwise() |>
-  mutate(BDF_median = mean(c_across( starts_with("forecast_")), na.rm = TRUE)) |>
+df_BDF_means <- stats_BDF |> 
+  mutate(BDF_mean = mean(median_forecast), na.rm = TRUE) |>
   select(Date, BDF_mean)
 
-df_INSEE_means <- wide_INSEE |> 
-  rowwise() |>
-  mutate(INSEE_median = mean(c_across( starts_with("forecast_")), na.rm = TRUE)) |>
+df_INSEE_means <- stats_INSEE |> 
+  mutate(INSEE_mean = mean(median_forecast), na.rm = TRUE) |>
   select(Date, INSEE_mean)
 
 df_cor_means <- inner_join(df_BDF_means, df_INSEE_means, by = "Date")
@@ -69,12 +96,8 @@ correlation_mean <- cor(df_cor_means$BDF_mean, df_cor_means$INSEE_mean, method =
 
 
 # T-Test sur les moyennes (variance inégale)
-t_test_results <- tryCatch({
-  t.test(df_cor_means$BDF_mean, df_cor_means$INSEE_mean, var.equal = FALSE)
-}, error = function(e) {
-  message("Erreur lors du t-test (probablement trop peu de données).")
-  return(NULL)
-})
+t_test_results <- t.test(df_cor_means$BDF_mean, df_cor_means$INSEE_mean, var.equal = FALSE)
+
 
 #############################################
 # Représentations graphique
@@ -82,10 +105,11 @@ t_test_results <- tryCatch({
 
 
 #  Boxplot 
-plot_boxplot <- ggplot(both_long, aes(x = source, y = as.numeric(forecast), fill = source)) +
-  geom_boxplot(alpha = 0.7, outlier.shape = 16, outlier.size = 1.5) +
-  # Une facette par date
-  facet_wrap(~ Date, scales = "free_y") + 
+boxplot <- ggplot(stats_both, aes(x = source, y = as.numeric(median_forecast), fill = source)) +
+  geom_boxplot(alpha = 0.7, 
+               outlier.shape = NA) + 
+  # facet par mois
+  facet_wrap(~ Mois_Label, scales = "free_y") + 
   labs(
     title = paste("Boxplots de Prévisions (", MODEL_NAME, ") : BDF vs INSEE"),
     y = "Prévision",
@@ -93,21 +117,22 @@ plot_boxplot <- ggplot(both_long, aes(x = source, y = as.numeric(forecast), fill
     fill = "Source"
   ) +
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_y_continuous(limits = c(-2, 2))
 
-print(plot_boxplot)
+print(boxplot)
 
-# Distribution
-plot_density <- ggplot(both_long, aes(x = as.numeric(forecast), fill = source, color = source)) +
+#distribution
+density <- ggplot(stats_both, aes(x = as.numeric(median_forecast), fill = source)) +
   geom_density(alpha = 0.4) +
-  # Une facette par date
-  facet_wrap(~ Date, scales = "free") + 
+  facet_wrap(~ Mois_Label, scales = "free") + 
   labs(
     title = paste("Distributions de Prévisions (", MODEL_NAME, ") : BDF vs INSEE"),
     x = "Prévision",
     y = "Densité",
     fill = "Source"
   ) +
-  theme_minimal()
+  theme_minimal() +
+  scale_x_continuous(limits = c(-1, 1.5))
 
-print(plot_density)
+print(density)
