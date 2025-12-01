b5a9223936d6d6b0865b771e2e99e5c5d1c21560
mbessec <60462278+mbessec@users.noreply.github.com>
2025-12-01 15:19:28 +0100

    j'ai retiré la correction erronée de BDF_sent qui mélangeait des prév et du sentiment

--- Ajout_Results2020.R
+++ Ajout_Results2020.R
@@ -64,19 +64,6 @@ write.xlsx(df_final_INSEE, file = "Final_Results/INSEE_Text_2020.xlsx",
 
 
 #============================================
-# Correction BDF_sentiment
-df_BDF <- read_excel('Results/BDF_sentiment.xlsx')   #1
-
-# remplace les dates communes de 2020 et ajoute les dates manquantes
-df_final_BDF <- df_BDF %>%
-  rows_upsert(df_BDF_2020, by = "Date") %>%
-  arrange(Date)
-
-write.xlsx(df_final_BDF, file = "Final_Results/BDF_sentiment_2020.xlsx", 
-           sheetName = 'prevision', rowNames = FALSE)  #3
-
-
-#============================================
 # Correction BDF_text_FR et INSEE_text_FR
 df_BDF <- read_excel('Results/BDF_text_FR.xlsx')   #1
 df_INSEE <- read_excel('Results/INSEE_text_FR.xlsx')   #2
