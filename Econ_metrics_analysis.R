51bdbf63592418f4e629c442b94f880a50e221d1
Victor Chabrol <victor1chabrol@gmail.com>
2025-12-02 17:23:19 +0100

    Adding metrics for AR and ISMA Sent

--- Econ_metrics_analysis.R
+++ Econ_metrics_analysis.R
@@ -1,7 +1,9 @@
 # Calcul MAE et RMSE de ISMA et AR(2)
 
+source("Library_Nowcasting_LLM.R")
 source("Modèle_ISMA.R")
 source("LLM_AR.R")
+source("LLM_sent_ISMA.R")
 
 ##################
 # COVID
@@ -13,6 +15,11 @@ if (COVID == 1){
     filter(dates <  "2020-02-01" | dates >= "2022-02-01") 
   df_AR <- df_AR |>
     filter(dates < "2020-02-01" | dates >= "2022-02-01") 
+  df_ISMA_sent <- df_ISMA_sent |>
+    filter(dates < "2020-02-01" | dates >= "2022-02-01")
+  df_AR_Sent <- df_AR_Sent |> 
+    filter(dates < "2020-02-01" | dates >= "2022-02-01")
+
 }
 
 #################################
@@ -47,12 +54,38 @@ metrics_AR <- df_AR |>
   ) |>
   select(-Global_MAE, -Global_RMSE)
 
+
+metrics_Sent_ISMA <- df_ISMA_sent |>
+  filter(dates >= "2010-02-01") |>
+  summarise(
+    Model = "Sentiment ISMA",
+    MAE_M1 = mean(abs(PIB_PR - forecast_M1 ), na.rm = TRUE),
+    MAE_M2 = mean(abs(PIB_PR - forecast_M2), na.rm = TRUE),
+    MAE_M3 = mean(abs(PIB_PR - forecast_M3), na.rm = TRUE),
+    RMSE_M1 = sqrt(mean((PIB_PR - forecast_M1)^2, na.rm = TRUE)),
+    RMSE_M2 = sqrt(mean((PIB_PR - forecast_M2)^2, na.rm = TRUE)),
+    RMSE_M3 = sqrt(mean((PIB_PR - forecast_M3)^2, na.rm = TRUE))
+  )
+  
+metrics_AR_Sent <- df_AR_Sent |>
+  filter(dates >= "2010-02-01") |>
+  summarise(
+    Model = "Sentiment AR(2)",
+    MAE_M1 = mean(abs(PIB_PR - forecast_AR_M1 ), na.rm = TRUE),
+    MAE_M2 = mean(abs(PIB_PR - forecast_AR_M2), na.rm = TRUE),
+    MAE_M3 = mean(abs(PIB_PR - forecast_AR_M3), na.rm = TRUE),
+    RMSE_M1 = sqrt(mean((PIB_PR - forecast_AR_M1)^2, na.rm = TRUE)),
+    RMSE_M2 = sqrt(mean((PIB_PR - forecast_AR_M2)^2, na.rm = TRUE)),
+    RMSE_M3 = sqrt(mean((PIB_PR - forecast_AR_M3)^2, na.rm = TRUE))
+  )
+
+
 ##############################
 #Tableau recap
 ##################
 
 
-econ_metrics_table <- bind_rows(metrics_ISMA, metrics_AR) |>
+econ_metrics_table <- bind_rows(metrics_ISMA, metrics_AR, metrics_Sent_ISMA) |>
   pivot_longer(
     cols = -Model, 
     names_to = c(".value", "Mois"),
@@ -78,6 +111,7 @@ cutoff_date <- as.Date("2020-02-01")
 #ISMA
 
 metrics_ISMA_period <- df_ISMA |>
+  filter(dates >= "2015-02-01") |>
   mutate(
     Period = case_when(
       dates < cutoff_date ~ "2015_2019",
@@ -101,6 +135,7 @@ metrics_ISMA_period <- df_ISMA |>
 # AR
 
 metrics_AR_period <- df_AR |>
+  filter(dates >= "2015-02-01") |>
   mutate(
     Period = case_when(
       dates < cutoff_date ~ "2015_2019",
@@ -122,7 +157,53 @@ metrics_AR_period <- df_AR |>
   ) |>
   select(-Global_MAE, -Global_RMSE)
 
+
+#Sentiment ISMA
+
+
+metrics_Sent_ISMA_period <- df_ISMA_sent |>
+  filter(dates >= "2015-02-01") |>
+  mutate(
+    Period = case_when(
+      dates < cutoff_date ~ "2015_2019",
+      dates >= cutoff_date ~ "2020_2025"
+    )
+  ) |>
+  group_by(Period) |>
+  summarise(
+    Model = "Sentiment ISMA",
+    # Calcul MAE
+    MAE_M1 = mean(abs(PIB_PR - forecast_M1), na.rm = TRUE),
+    MAE_M2 = mean(abs(PIB_PR - forecast_M2), na.rm = TRUE),
+    MAE_M3 = mean(abs(PIB_PR - forecast_M3), na.rm = TRUE),
+    # Calcul RMSE
+    RMSE_M1 = sqrt(mean((PIB_PR - forecast_M1)^2, na.rm = TRUE)),
+    RMSE_M2 = sqrt(mean((PIB_PR - forecast_M2)^2, na.rm = TRUE)),
+    RMSE_M3 = sqrt(mean((PIB_PR - forecast_M3)^2, na.rm = TRUE)),
+    .groups = "drop"
+  )
+
+metrics_AR_Sent_period <- df_AR_Sent |>
+  filter(dates >= "2015-02-01") |>
+  mutate(
+    Period = case_when(
+      dates < cutoff_date ~ "2015_2019",
+      dates >= cutoff_date ~ "2020_2025"
+    )
+  ) |>
+  group_by(Period) |>
+  summarise(
+    Model = "Sentiment AR(2)",
+    MAE_M1 = mean(abs(PIB_PR - forecast_AR_M1), na.rm = TRUE),
+    MAE_M2 = mean(abs(PIB_PR - forecast_AR_M2), na.rm = TRUE),
+    MAE_M3 = mean(abs(PIB_PR - forecast_AR_M3), na.rm = TRUE),
+    RMSE_M1 = sqrt(mean((PIB_PR - forecast_AR_M1)^2, na.rm = TRUE)),
+    RMSE_M2 = sqrt(mean((PIB_PR - forecast_AR_M2)^2, na.rm = TRUE)),
+    RMSE_M3 = sqrt(mean((PIB_PR - forecast_AR_M3)^2, na.rm = TRUE)),
+    .groups = "drop"
+  )
+
 #Tableau recap
-econ_metrics_period <- bind_rows(metrics_ISMA_period, metrics_AR_period)
+econ_metrics_period <- bind_rows(metrics_ISMA_period, metrics_AR_period, metrics_Sent_ISMA_period, metrics_AR_Sent_period )
 
 
